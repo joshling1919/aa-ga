546f6ed40534b07efecbd66b4e35e006eec43c64
Joshua Ling <joshling1919@gmail.com>
2017-03-18 01:35:22 -0700

    Change to only use as script

--- input.js
+++ input.js
@@ -1,18 +1,44 @@
-function insertProperty(websiteURL, name) {
+ALL_PROPERTIES = [
+  {
+    websiteURL: 'www.test1.com',
+    name: 'test1'
+  },
+  {
+    websiteURL: 'www.test2.com',
+    name: 'test2'
+  }
+];
+
+function insertProperty(property) {
+  debugger;
   var request = gapi.client.analytics.management.webproperties.insert(
     {
       'accountId': '91940833',
       'resource': {
-        'websiteUrl': websiteURL,
-        'name': name
+        'websiteUrl': property.websiteURL,
+        'name': property.name
       }
     });
-  request.execute(function (response) { console.log(response) });
+  request.execute(function (response) {
+    if (response.error) {
+      console.log('error happened in inserting!');
+      console.log(response);
+    }
+    else {
+      console.log('insert happened!');
+      console.log(response);
+    }
+  });
 }
 
-function handleSubmit(event) {
-  debugger;
+function insertAllProperties(properties) {
+  return function(event) {
+    event.preventDefault();
+    properties.forEach(function(property) {
+      insertProperty(property);
+    });
+  }
 }
 
-var insert = document.getElementById('insert');
-insert.addEventListener('submit', handleSubmit);
+
+document.getElementById('run').addEventListener('click', insertAllProperties(ALL_PROPERTIES));
